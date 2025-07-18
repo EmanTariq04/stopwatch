0de59bf66c830700a0b4bca865cb76983533de6a
Eman Tariq <emantariq.1004@gmail.com>
2025-07-18 22:42:58 +0500

    final commit

--- script.js
+++ script.js
@@ -8,18 +8,49 @@ let elapsedTime = 0;
 let timerInterval = 0;
 
 function startTimer() {
-    startTime = Date.now() - elapsedTime
+  startTime = Date.now() - elapsedTime;
 
-    timerInterval = setInterval(() => {
-        elapsedTime = Date.now() - startTime;
-        timerEl.textContent = elapsedTime;
-    }, 10);
+  timerInterval = setInterval(() => {
+    elapsedTime = Date.now() - startTime;
+    timerEl.textContent = formatTime(elapsedTime);
+  }, 10);
+
+  startButtonEl.disabled = true;
+  stopButtonEl.disabled = false;
 }
 
-function stopTimer() {}
+formatTime();
 
-function resetTimer() {}
+function formatTime(elapsedTime) {
+  const milliseconds = Math.floor((elapsedTime % 1000) / 10);
+  const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+  const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+  const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
 
+  return (
+    (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
+    ":" +
+    (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
+    ":" +
+    (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") +
+    "." +
+    (milliseconds > 9 ? milliseconds : "0" + milliseconds)
+  );
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
+  startButtonEl.disabled = false;
+  stopButtonEl.disabled = true;
+}
+
+function resetTimer() {
+  clearInterval(timerInterval);
+  elapsedTime = 0;
+  timerEl.textContent = "00:00:00"
+  startButtonEl.disabled = false;
+  stopButtonEl.disabled = false;
+}
 
 startButtonEl.addEventListener("click", startTimer);
 stopButtonEl.addEventListener("click", stopTimer);
